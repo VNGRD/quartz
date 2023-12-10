982b59ceacf0641ddac038645ba851ba6bfb6b17
VNGRD <49817810+VNGRD@users.noreply.github.com>
2023-12-10 05:05:44 -0800

    oops site broke

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "vngrd.github.io/sprout",
+    baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
