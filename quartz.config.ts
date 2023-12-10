1ed4dd65d407938c57b464160c0b10f03929b2f4
VNGRD <49817810+VNGRD@users.noreply.github.com>
2023-12-10 06:00:26 -0800

    revert font after pages fix

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,8 +14,8 @@ const config: QuartzConfig = {
     defaultDateType: "created",
     theme: {
       typography: {
-        header: "Garamond",
-        body: "Garamond",
+        header: "Nunito",
+        body: "Nunito",
         code: "Courier",
       },
       colors: {
