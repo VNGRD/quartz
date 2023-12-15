2407c21cce5e1ef905a62acf2a1c2af4f95d3b5c
VNGRD <49817810+VNGRD@users.noreply.github.com>
2023-12-14 16:55:47 -0800

    Update quartz.layout.ts
    disabled contentmeta component since it for some reason displays dates incorrectly.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -22,7 +22,7 @@ export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
-    Component.ContentMeta(),
+   //Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
